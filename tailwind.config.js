28039024f3349d22f965485c9a18243d2e9c2890
elderfausto <elderfavsto@gmail.com>
2025-11-10 13:31:38 -0300

    build(frontend): Initial setup of Vue.js project with Vite and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Adiciona a extensão .vue
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
